f516d3cbf18017c664119c2e97daccdfbb136e44
AbeTavarez <abeincwireless@gmail.com>
2021-06-20 18:45:10 -0400

    Coordinate class

--- Coordinate.py
+++ Coordinate.py
@@ -1,2 +1,9 @@
 
-# First value
+# Coordinate is our Genesis Block.
+# All Paths connects are connected to the Coordinate. 
+
+class Coordinate():
+    def __init__(self, previous_hash, index, transactions) -> None:
+        previous_hash = ''
+        index = 0
+        transactions = list()
